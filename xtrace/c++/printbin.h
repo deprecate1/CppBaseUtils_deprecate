193e2eb04e21c60bd47eae3b2671a9c14592a38b
wjx0912 <weiqi5@qq.com>
2016-07-21 17:08:44 +0800

    printbin fix

--- xtrace/c++/printbin.h
+++ xtrace/c++/printbin.h
@@ -6,18 +6,37 @@
 
 #define PRINT_BYTE_PER_LINE		16
 #ifdef DEBUG_PRINT_BIN
+
+static inline void __printline(COLORREF textCr, int addr, void *buffer, int len)
+{
+	TCHAR outstr[256] = L"";
+	TCHAR temp[8] = L"";
+	wsprintf(outstr, L",0X%04X: ", addr);
+	for (int i = 0; i < len; i++) {
+		wsprintf(temp, L"%02X ", ((unsigned char *)buffer)[i]);
+		wcscat(outstr, temp);
+	}
+	wcscat(outstr, L"\n");
+	XTRACE(textCr, outstr);
+}
+
+static inline void printbin(COLORREF textCr, void *buffer, int len)
+{
+	//XTRACE(textCr, _T("\n"));
+	int i;
+	for (i = 0; i < len; i += PRINT_BYTE_PER_LINE) {
+		int leave = len - i;
+		if (leave >= PRINT_BYTE_PER_LINE)
+			__printline(textCr, i, (char *)buffer + i, PRINT_BYTE_PER_LINE);
+		else
+			__printline(textCr, i, (char *)buffer + i, leave);
+	}
+	//XTRACE(textCr, _T("\n"));
+}
+
 static inline void printbin(void *buffer, int len)
 {
-	XTRACE(RGB(255, 0, 0), _T("\n"));
-
-	int i;
-	for(i = 0; i < len; i++) {
-		if(0 != i && 0 == i % PRINT_BYTE_PER_LINE)
-			XTRACE(RGB(255, 0, 0), _T("\n"));
-
-		XTRACE(RGB(255, 0, 0), _T("%02X "), ((unsigned char *)buffer)[i]);
-	}
-	XTRACE(RGB(255, 0, 0), _T("\n"));
+	printbin(RGB(0, 0, 0), buffer, len);
 }
 #else
 #define printbin(buffer, len)
