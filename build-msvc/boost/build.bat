fb230124f5a90c82d9bce64daec8c5d936987bfb
wyt <80101277@qq.com>
2023-05-22 08:57:00 +0800

    fix boost build

--- build-msvc/boost/build.bat
+++ build-msvc/boost/build.bat
@@ -1,16 +1,16 @@
 @echo off
-set boost_dir="D:/sdk/vs2019/boost_1_77_0"
+set boost_dir="D:/sdk/props_build_vs2022/boost_1_82_0"
 
 cd /d %boost_dir%
 call "bootstrap.bat"
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
-b2.exe   toolset=msvc-14.2    --prefix=./build/x64-default/prefix      --stagedir=./build/x64-default/stage       --build-dir=./build/x64-default/build
-b2.exe   toolset=msvc-14.2    --prefix=./build/x64-static/prefix         --stagedir=./build/x64-static/stage          --build-dir=./build/x64-static/build         link=static   runtime-link=static
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+b2.exe   toolset=msvc-14.3    --prefix=./build/x64-default/prefix      --stagedir=./build/x64-default/stage       --build-dir=./build/x64-default/build
+b2.exe   toolset=msvc-14.3    --prefix=./build/x64-static/prefix       --stagedir=./build/x64-static/stage        --build-dir=./build/x64-static/build         link=static   runtime-link=static
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
-b2.exe   toolset=msvc-14.2    --prefix=./build/x86-default/prefix      --stagedir=./build/x86-default/stage       --build-dir=./build/x86-default/build
-b2.exe   toolset=msvc-14.2    --prefix=./build/x86-static/prefix         --stagedir=./build/x86-static/stage          --build-dir=./build/x86-static/build         link=static   runtime-link=static
+call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars32.bat"
+b2.exe   toolset=msvc-14.3    --prefix=./build/x86-default/prefix      --stagedir=./build/x86-default/stage       --build-dir=./build/x86-default/build
+b2.exe   toolset=msvc-14.3    --prefix=./build/x86-static/prefix       --stagedir=./build/x86-static/stage        --build-dir=./build/x86-static/build         link=static   runtime-link=static
 
 rm  -fr  .\build\x86-default\build\
 rm  -fr  .\build\x86-static\build\
